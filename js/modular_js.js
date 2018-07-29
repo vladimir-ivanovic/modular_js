abe187d9ec73e479eb66e5699b0ba488c55b7ebb
jahorinac1 <vladimir.codeplicity@gmail.com>
2018-07-29 12:29:00 +0200

    modified js file for Revealing Module Pattern

--- js/modular_js.js
+++ js/modular_js.js
@@ -1,69 +1,48 @@
-/*
+var people = (function(){
+    var people = ['Will', 'Steve'];
 
-Some ground rules:
+    //cache DOM
+    var $el = $('#peopleModule');
+    var $button = $el.find('button');
+    var $input = $el.find('input');
+    var $ul = $el.find('ul');
+    var template = $el.find('#people-template').html();
 
-- Self-contained module
-    - everything to do with my module is in my module
-    - no global variable
-    - if a module manages more than one think it should be slit up
-- sepration of concerns
-- DRY code: Don't Repeat Yourself
-- efficient DOM usage
-    - very few $(selections)
-- no memory leaks
-- all events can be unbound
+    //bind events
+    $button.on('click', addPerson);
+    $ul.delegate('i.del', 'click', deletePerson);
 
-*/
+    _render();
 
-
-/*** Example of spaghetti code ***/
-
-var people = [];
-var template = $('#people-template').html();
-
-$('#peopleModule').find('button').on('click', function () {
-    people.push($('#peopleModule').find('input').val());
-    $('#peopleModule').find('input').val('');
-
-    var data = {
-        people:people,
-    };
-    $('#peopleModule').find('ul').html(Mustache.render(template, data));
-});
-
-$('#peopleModule').find('ul').delegate('i.del', 'click', function (e) {
-   var $remove = $(e.target).closest('li');
-   var i = $('#peopleModule').find('ul').find('li').index($remove);
-
-   $remove.remove();
-
-   people.splice(i, 1);
-});
-
-
-
-
-//object literal
-//  !!! My API has two methods
-// The methods that you expose to other modules the things that allow modules to do actions are called your API.
-// So currently my API has two methods, it has sayName and it has set name, so I could actually hack this.
-// To hide some of these thinks (modul methods) away to where nobody else can access them only the module itself can access them.
-// That's kind of the modular pattern !!!
-
-
-
-
-var myModule = {
-    name : 'Will',
-    age: 34,
-    sayName : function () {
-        alert(this.name);
-    },
-    setName : function (newName) {
-        this.name = newName;
+    function _render() {
+        $ul.html(Mustache.render(template, {people: people}));
     }
-}
 
-myModule.setName('Pero');
-myModule.sayName();
+    function addPerson(value) {
+        var name = (typeof value === "string") ? value : $input.val();
+        people.push(name);
+        _render();
+        $input.val('');
+    }
 
+    function deletePerson(event) {
+        var i;
+        if (typeof event === "number") {
+            i = event;
+        } else {
+            var $remove = $(event.target).closest('li');
+            i = $ul.find('li').index($remove);
+        }
+        people.splice(i, 1);
+        _render();
+    }
+
+    return {
+        addPerson: addPerson,
+        deletePerson: deletePerson
+    };
+
+})();
+
+//people.addPerson("Jake");
+//people.deletePerson(0);
